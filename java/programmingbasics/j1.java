daf445503be8826475f72e36a930d10ddc6563ec
pratham-sugandhi <psugandhi08@gmail.com>
2025-03-05 19:01:11 +0530

    Add files via upload
    Java Beginning - Datatype and Operations d1

---
+++ java/programmingbasics/j1.java
@@ -0,0 +1,89 @@
+class j1
+{
+    public static void main(String args[])
+    {
+// Hello World-
+        // System.out.println("Helloo");
+
+
+// Variable-
+        // int num1 = 3;
+        // int num2 = 5;
+        // int res = num1 + num2;
+        // System.out.println(res);
+
+
+// DATA TYPES-
+        // int num = 3;
+        // byte by = 127;
+        // short sh = 453;
+        // long l =4953l;
+
+        // float marks = 6.5f;
+        // double d = 5.7;
+        
+        // char c = 'c';
+
+        // boolean b = true;
+
+
+//literals- 3,127,...
+
+//Type Conversion and Casting
+        // int a = 257;
+        // byte k = (byte) a;
+
+        // float f =758.89f;
+        // int t = (int) f;
+
+        // System.out.println(k);  // 257% = 257/256
+        // System.out.println(t);
+
+// Type Promotions
+        // byte a = 10;
+        // byte b = 30;
+
+        // int res = a * b;
+        // System.out.println(res);
+
+
+// ARITHMETIC OPERATION
+        // int num1 = 78;
+        // int num2 = 433;
+        // int res = num1 + num2;
+        // int res = num1 - num2;
+        // int res = num1 * num2;
+        // int res = num1 / num2;
+        // int res = num1 % num2;
+
+        // num1 = num1 + 2;
+        // num += 2;
+        // num -= 2;
+
+        //num1 = num1 + 1;
+        // num++;    //increment
+        // ++num;
+        // num--;    //decrement
+        // --num;
+
+        // System.out.println(num1);
+    
+
+// RELATIONAL OPERATION
+        // int x = 5;
+        // int y = 3;
+        // boolean res = x != y;
+
+        // System.out.println(res);
+
+// LOGICAL OPERATION
+        // int x = 5;
+        // int y = 8;
+        // int a = 3;
+        // int b = 9;
+
+        // boolean res = x>y || a<b;
+
+        // System.out.println(!res);
+    }
+}
