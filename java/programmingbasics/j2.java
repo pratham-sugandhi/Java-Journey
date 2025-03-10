82e09427ef9573a134f4e82670f7f32e07ed51e2
pratham <pdsugandhi08@gmail.com>
2025-03-10 20:36:17 +0530

    Day-6 Static Keyword

--- java/programmingbasics/j2.java
+++ java/programmingbasics/j2.java
@@ -6,46 +6,89 @@ class Student
     int marks;
 }
 
+class Mobile
+{
+    String brand;
+    int price;
+    static String name;
+
+    static
+    {    
+        name = "Phone";
+        System.out.println("in static block");
+    }
+
+    public Mobile()
+    {
+        brand = "";
+        price = 10000;
+        System.out.println("in constuctor block");
+    }
+
+    public void show()
+    {
+        System.out.println(brand + " : " + price  + " : " + name);
+    }
+
+    public static void show1(Mobile obj)
+    {
+        // System.out.println(obj.brand + " : " + obj.price  + " : " + obj.name):
+    }
+}
+
+import java.util.Scanner;
+
 public class j2
 {
     public static void main(String args[])
     {
 
-        int nums[] = new int[4];
-        nums[0] = 4;
-        nums[1] = 3;
-        nums[2] = 9;
-        nums[3] = 7;
+        Scanner sc = new Scanner(System.in);
 
-        for(Student stud : students)
-        {
-            System.out.println(stud.name + " : " + stud.mark);
-        }
+        int radius;
+        radius = sc.nextInt();
+        double pi = 3.1415;
+        double area = pi * radius * radius;
+        // Systebsbkfbm.out.println(area);
 
-        Student s1 = new student();
-        s1.rollno = 1;
-        s1.name = "Rahul";
-        s1.marks = 89;
 
-        Student s2 = new student();
-        s2.rollno = 2;
-        s2.name = "Mohit";
-        s2.marks = 98;
+// ARRAY -2
+        // int nums[] = new int[4];
+        // nums[0] = 4;
+        // nums[1] = 3;
+        // nums[2] = 9;
+        // nums[3] = 7;
 
-        Student s3 = new student();
-        s3.rollno = 3;
-        s3.name = "Nigam";
-        s3.marks = 83;
+        // for(Student stud : students)
+        // {
+        //     System.out.println(stud.name + " : " + stud.mark);
+        // }
 
-        Student students[] = new Student[3];
-        students[0] = s1;
-        students[1] = s2;
-        students[2] = s3;
+        // Student s1 = new Student();
+        // s1.rollno = 1;
+        // s1.name = "Rahul";
+        // s1.marks = 89;
 
-        for(int i=0; i<students.length; i++)
-        {
+        // Student s2 = new Student();
+        // s2.rollno = 2;
+        // s2.name = "Mohit";
+        // s2.marks = 98;
+
+        // Student s3 = new Student();
+        // s3.rollno = 3;
+        // s3.name = "Nigam";
+        // s3.marks = 83;
+
+        // Student students[] = new Student[3];
+        // students[0] = s1;
+        // students[1] = s2;
+        // students[2] = s3;
+
+        // for(int i=0; i<students.length; i++)
+        // {
             // System.out.println(students[i].name + " : " + students[i].marks);
-        }
+        // }
+
 // ARRAYS-
         // int nums[][] = new int [3][4];
 
@@ -98,6 +141,54 @@ public class j2
         //     System.out.println();
         // }
     
+// STRINGS- //immutable
+
+        // String name = new String("navin");
+        String name = "naveen";
+        name = name + "reddy";     //new object 
+
+        String s1 = "navin";
+        String s2 = "navin";    //refrence
+
+        // System.out.println("hello " + name);
+        // System.out.println(name.charAt(3));
+        // System.out.println(name.cpncat(" reddy"));
+
+// TYPES - mutable & immutable
+        //String Buffer-thread safe
+        StringBuffer sb = new StringBuffer("naveen");
+        sb.append(" reddy");
+        sb.deleteCharAt(2);
+        sb.insert(6, "java");
+        sb.setLength(30);
+
+        // System.out.println(sb);
+        // System.out.println(sb.capacty());
+        // System.out.println(sb.length());
+
+// Static- can be called with thier class name directly without object
+// Non static need classes object to be called
+
+    // Mobile obj1 = new Mobile();
+
+    // obj1.brand = "Apple";
+    // obj1.price = 10000;
+    // Mobile.name = "Phone";
+    
+    // obj1.show();
+    // Mobile.show1(obj1);
+
+    // Mobile obj3 = new Mobile();
+
+    // obj3.brand = "VIVO";
+    // obj3.price = 10000;
+    // Mobile.name = "SmartPhone";
+    
+    // obj3.show();
+
+
+    // Class.forName("Mobile"); //will call static block
+
     }
 
 }
