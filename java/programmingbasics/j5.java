576481c0f48112bb30d44e0d25a97ba83852830f
pratham <pdsugandhi08@gmail.com>
2025-03-21 16:02:52 +0530

    Day-18 Stream API

--- java/programmingbasics/j5.java
+++ java/programmingbasics/j5.java
@@ -195,56 +195,167 @@ public class j5{
 
 // COMPARATOR VS COMPARABLE-
     // Comparator- own logic for sorting
-
-        Comparator<Integer> com = new Comparator<Integer>()
-        {
-            public int compare(Integer i, Integer j){
-                if(i%10 > j%10)
-                    return 1;   //swap
-                else
-                    return -1;
-            }
-        };
-
-        List<Integer> nums = new ArrayList<>();
-        nums.add(43);
-        nums.add(28);
-        nums.add(76);
-        nums.add(50);
-
-        Collection.sort(nums);
-        Collection.sort(nums, com);
-
-        System.out.println(nums);
-
-
-        Comparator<Student> com2 = new Comparator<Student>()
-        {
-            public int compare(Student i, Student j){
-                if(i.age > j.age)
-                    return 1;   //swap
-                else
-                    return -1;
-            }
-        };
-
-        List<Student> studs = new ArrayList<>();
-        studs.add(new Student(19, "prathu"));
-        studs.add(new Student(21, "rathu"));
-        studs.add(new Student(17, "brathu"));
-        studs.add(new Student(19, "sathu"));
-        
-        Collection.sort(studs, com2);
-
-        for(Student s : studs)
-            System.out.println(s);
-
     // Comparable- power to compare itself
-//
+
+        // Comparator<Integer> com = new Comparator<Integer>()
+        // {
+        //     public int compare(Integer i, Integer j){
+        //         if(i%10 > j%10)
+        //             return 1;   //swap
+        //         else
+        //             return -1;
+        //     }
+        // };
+
+        // List<Integer> nums = new ArrayList<>();
+        // nums.add(43);
+        // nums.add(28);
+        // nums.add(76);
+        // nums.add(50);
+
+        // Collection.sort(nums);
+        // Collection.sort(nums, com);
+
+        // System.out.println(nums);
+
+
+
+        // Comparator<String> com3 = new Comparator<String>(){
+        //     public String compare(String i, String j){
+        //         if(i.stringLength() > j.stringLength())
+        //             return 1;   //swap
+        //         else
+        //             return -1;
+        //     }
+        // };
+
+
+        // List<String> numms = new ArrayList<>();
+        // numms.add("prathu");
+        // numms.add("rathu");
+        // numms.add("pathu");
+        // numms.add("prthu");
+
+        // Collection.sort(numms);
+        // Collection.sort(numms, com3);
+
+
+
+        // // Comparator<Student> com2 = new Comparator<Student>()
+        // // {
+        // //     public int compare(Student i, Student j){
+        // //         if(i.age > j.age)
+        // //             return 1;   //swap
+        // //         else
+        // //             return -1;
+        // //     }
+        // // };
+
+        // Comparator<Student> com2 = (Student i, Student j) -> 
+        //                 i.age > j.age ? 1 : -1;
+
+        // List<Student> studs = new ArrayList<>();
+        // studs.add(new Student(19, "prathu"));
+        // studs.add(new Student(21, "rathu"));
+        // studs.add(new Student(17, "brathu"));
+        // studs.add(new Student(19, "sathu"));
+        
+        // Collection.sort(studs, com2);
+
+        // for(Student s : studs)
+        //     System.out.println(s);
+
+
+// STREAM API-
+    // Filter, Map , Reduce , forEach
+    // List<Integer> nums = Arrays.asList(4,9,6,2,8);
+
+    // for(int i=0; i<nums.size(); i++){
+    //     System.out.println(nums.get(i));
+    // };
+
+    // for(int n : nums){
+    //     System.out.println(n);
+    // };
+
+    // nums.forEach(n -> System.out.println(n));
+
+    // int sum = 0;
+    // for(int n : nums){
+    //     if(n%2==0){
+    //         n = n*2;
+    //         sum = sum + n;
+    //     } 
+    // }
+    // System.out.println(nums);
+
+    // STREAM-
+    // List<Integer> nums = Arrays.asList(8,5,6,3,9);
+
+    // Stream<Integer> s1 = nums.Stream();
+    // Stream<Integer> s2 = s1.filter(n -> n%2 == 0);
+    // Stream<Integer> s3 = s2.map(n -> n*2);
+
+    // int result = nums.Stream()
+    //             .filter(n -> n%2==0)
+    //             .map(n -> n*2)
+    //             .reduce(0, (c,e) -> c+e);
+
+    // System.out.println(result);
+
+    // int res = s3.reduce(0, (c,e) -> c+e);
+    // System.out.println(res);
+
+    // s1.forEach(n -> System.out.println(n));
+    // s2.forEach(n -> System.out.println(n));
+    // s3.forEach(n -> System.out.println(n));
+    // s1.forEach(n -> System.out.println(n)); //cant repeat
+    // nums.forEach(n -> System.out.println(n));
+
+// STREAM API 2-
+    // List<Integer> nums = Arrays.asList(8,5,6,3,9);
+
+    // Predicate<Integer> p = new Predicate<Integer>(){
+    //     public boolean test(Integer n) {
+    //         if(n%2==0)
+    //             return true;
+    //         else
+    //             return false;
+    //     }
+    // };
+    // Predicate<Integer> p = n -> n%2==0;
+
+    // Function<Integer, Integer> fun = new Function<Integer, Integer>() {
+    //     public Integer apply(Integer n) {
+    //         return n*2; 
+    //     }
+    // };
+    // Function<Integer, Integer> fun = n -> n*2;
+    
+
+    // int result = nums.Stream()
+    //             .filter(p)      //n -> n%2==0;
+    //             .map(fun)       //n -> n*2;
+    //             .reduce(0, (c,e) -> c+e);   //
+
+    // System.out.println(result);
+
+    // Stream filteredValue = nums.parallelStream()
+    //             .filter(p)
+    // filteredValue.forEach(n -> System.out.println(n));
+
+    // Stream sortedValues = nums.Stream()
+    //             .filter(p)
+    //             .sorted();
+
+    // sortedValues.forEach(n -> System.out.println(n));
+
+// 
     }
 }
 
-// class Student{
+// class Student implements Comparable<Student>
+// {
 //     int age;
 //     String name;
 
@@ -256,8 +367,14 @@ public class j5{
 //     public String toString(){
 //         return "Std [age=" + age +", name="+ name + "]"
 //     }
-// }
 
+//     public int compareTo(Student that){
+//         if(this.age > this.age)
+//             return 1;
+//         else 
+//             return -1;
+//     }
+// }
 
 // class Counter
 // {
